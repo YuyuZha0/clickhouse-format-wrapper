b23f695667b58b80a5816c1a07726c19320430d5
yuyu.zhao <yuyu.zhao@getjerry.com>
2023-04-07 00:04:09 +0800

    add hilite support

--- src/main/resources/webroot/index.js
+++ src/main/resources/webroot/index.js
@@ -2,13 +2,11 @@
 
     $(document).ready(function () {
 
-        new Clipboard('#copyBtn');
-
         const $input = $('#sqlInput');
         const $output = $('#sqlOutput');
 
         const resetHeight = function () {
-            const height = Math.round($(window).height() * 0.8) + 'px'
+            const height = Math.round($(window).height() * 0.75) + 'px'
             $input.css('height', height);
             $output.css('height', height);
         };
@@ -27,7 +25,7 @@
         const $maxQuerySize = $('#maxQuerySizeInput');
         const $maxParserDepth = $('#maxParserDepthInput');
 
-        const buildOptions = function () {
+        const getOptions = function () {
             const obj = {
                 hilite: $hilite.prop('checked'),
                 oneline: $oneline.prop('checked'),
@@ -44,32 +42,44 @@
                     delete obj[key];
                 }
             }
-            return $.param(obj);
+            return obj;
         };
+
+        const ansiUp = new AnsiUp();
+        const formatRequest = function () {
+            const sql = $input.val();
+            if (/^\s*$/.test(sql)) {
+                return;
+            }
+            $output.empty();
+            const options = getOptions();
+            $.ajax({
+                url: 'api/format?' + $.param(options),
+                method: 'POST',
+                data: sql,
+                dataType: 'text',
+                success: function (data) {
+                    if (typeof data === 'string' || data instanceof String) {
+                        if (options.hilite) {
+                            $output.html(ansiUp.ansi_to_html(data));
+                        } else {
+                            $output.html(`<pre><code>${data}</code></pre>`);
+                        }
+                    }
+                },
+                error: function (xhr, status, error) {
+                    $output.html(`<p class="text-danger">${status}: ${xhr.responseText}</p>`);
+                }
+            });
+        }
 
         let timer = null;
         $input.on('input', function () {
             if (timer) {
                 clearTimeout(timer);
             }
-            timer = setTimeout(function () {
-                const sql = $input.val();
-                if (/^\s+$/.test(sql)) {
-                    return;
-                }
-                $.ajax({
-                    url: 'api/format?' + buildOptions(),
-                    method: 'POST',
-                    data: sql,
-                    dataType: 'text',
-                    success: function (data) {
-                        $output.text(data);
-                    },
-                    error: function (xhr, status, error) {
-                        $output.text(status + ': ' + error);
-                    }
-                });
-            }, 200);
+            timer = setTimeout(formatRequest, 200);
         });
+        new Clipboard('#copyBtn');
     });
 })(window.jQuery);
