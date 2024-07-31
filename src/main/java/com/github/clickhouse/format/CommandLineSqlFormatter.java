e8119de9634de7d62cdf9e86b3209f13095e413b
yuyu.zhao <yuyu.zhao@getjerry.com>
2024-07-31 15:48:38 +0800

    optimize

--- src/main/java/com/github/clickhouse/format/CommandLineSqlFormatter.java
+++ src/main/java/com/github/clickhouse/format/CommandLineSqlFormatter.java
@@ -1,35 +1,24 @@
 package com.github.clickhouse.format;
 
 import com.google.common.io.ByteStreams;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufOutputStream;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.buffer.impl.VertxByteBufAllocator;
-import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public final class CommandLineSqlFormatter {
 
-  private static final ScheduledExecutorService WATCH_DOG =
-      Executors.newSingleThreadScheduledExecutor(
-          new ThreadFactoryBuilder()
-              .setNameFormat("clickhouse-format-watch-dog-%d")
-              .setDaemon(true)
-              .build());
   private static final File CLICKHOUSE_HOME;
   private static final String BIN_NAME = "clickhouse-format";
   private static final int MAX_CONCURRENT_FORMATTING = 10;
@@ -65,17 +54,6 @@ public final class CommandLineSqlFormatter {
     }
   }
 
-  private static ScheduledFuture<?> registerTimeout(Process process) {
-    return WATCH_DOG.schedule(
-        () -> {
-          if (process.isAlive()) {
-            process.destroy();
-          }
-        },
-        30,
-        TimeUnit.SECONDS);
-  }
-
   public static boolean isCommandAvailable() {
     try {
       ProcessBuilder processBuilder = new ProcessBuilder();
@@ -108,14 +86,14 @@ public final class CommandLineSqlFormatter {
     setClickhouseHome(processBuilder);
 
     Process process = processBuilder.start();
-    ScheduledFuture<?> timeoutFuture = registerTimeout(process);
     try (OutputStream outputStream = process.getOutputStream()) {
       outputStream.write(input.getBytes());
     }
-    int code = process.waitFor();
-    if (!timeoutFuture.isDone()) {
-      timeoutFuture.cancel(true);
+    if (!process.waitFor(30, TimeUnit.SECONDS)) {
+      process.destroy();
+      throw new SqlFormatException(-1, Buffer.buffer("Timeout"));
     }
+    int code = process.exitValue();
     if (code != 0) {
       try (InputStream inputStream = process.getErrorStream()) {
         throw new SqlFormatException(code, inputStreamToBuffer(inputStream));
