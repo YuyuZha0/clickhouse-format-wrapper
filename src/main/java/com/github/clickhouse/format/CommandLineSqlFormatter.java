96b7a1cf3ecc57968c1be7f7d036986f5e55c886
yuyu.zhao <yuyu.zhao@getjerry.com>
2025-02-19 14:23:33 +0800

    optimize

--- src/main/java/com/github/clickhouse/format/CommandLineSqlFormatter.java
+++ src/main/java/com/github/clickhouse/format/CommandLineSqlFormatter.java
@@ -1,14 +1,12 @@
 package com.github.clickhouse.format;
 
+import com.google.common.collect.Lists;
 import com.google.common.io.ByteStreams;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufOutputStream;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.buffer.impl.VertxByteBufAllocator;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Semaphore;
@@ -18,89 +16,98 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public final class CommandLineSqlFormatter {
-
-  private static final File CLICKHOUSE_HOME;
+  private static final File CLICKHOUSE_HOME = initClickhouseHome();
   private static final String BIN_NAME = "clickhouse-format";
   private static final int MAX_CONCURRENT_FORMATTING = 10;
+  private static final int TIMEOUT_SECONDS = 30;
+  private static final Semaphore semaphore = new Semaphore(MAX_CONCURRENT_FORMATTING);
 
-  static {
+  private static File initClickhouseHome() {
     String clickhouseHome = System.getenv("CLICKHOUSE_HOME");
     if (clickhouseHome != null && !clickhouseHome.isEmpty()) {
       File clickhouseHomeFile = new File(clickhouseHome);
       if (clickhouseHomeFile.exists() && clickhouseHomeFile.isDirectory()) {
-        CLICKHOUSE_HOME = clickhouseHomeFile;
-        log.info("CLICLHOUSE_HOME is set to \"{}\".", clickhouseHome);
-      } else {
-        CLICKHOUSE_HOME = null;
+        log.info("CLICKHOUSE_HOME is set to \"{}\".", clickhouseHome);
+        return clickhouseHomeFile;
       }
-    } else {
-      CLICKHOUSE_HOME = null;
     }
+    return null;
   }
-
-  private final Semaphore semaphore = new Semaphore(MAX_CONCURRENT_FORMATTING);
 
   private static Buffer inputStreamToBuffer(InputStream inputStream) throws IOException {
     ByteBuf byteBuf = VertxByteBufAllocator.DEFAULT.heapBuffer();
-    try (OutputStream outputStream = new ByteBufOutputStream(byteBuf)) {
+    try (ByteBufOutputStream outputStream = new ByteBufOutputStream(byteBuf)) {
       ByteStreams.copy(inputStream, outputStream);
+      return Buffer.buffer(byteBuf);
     }
-    return Buffer.buffer(byteBuf);
   }
 
-  private static void setClickhouseHome(ProcessBuilder processBuilder) {
+  private static ProcessBuilder createProcessBuilder(List<String> command) {
+    ProcessBuilder processBuilder = new ProcessBuilder(command);
+    processBuilder.redirectErrorStream(false);
     if (CLICKHOUSE_HOME != null) {
       processBuilder.directory(CLICKHOUSE_HOME);
     }
+    return processBuilder;
   }
 
   public static boolean isCommandAvailable() {
     try {
-      ProcessBuilder processBuilder = new ProcessBuilder();
-      processBuilder.command(BIN_NAME, "--query", "select 1");
-      processBuilder.redirectErrorStream(false);
-      setClickhouseHome(processBuilder);
-      Process process = processBuilder.start();
-      int code = process.waitFor();
-      return code == 0;
+      Process process =
+          createProcessBuilder(Lists.newArrayList(BIN_NAME, "--query", "select 1")).start();
+      return process.waitFor() == 0;
     } catch (Exception e) {
       return false;
     }
   }
 
   public Buffer format(@NonNull Buffer input, @NonNull Options options) throws Exception {
-
     try {
-      semaphore.acquire();
-      return format0(input, options);
-    } finally {
-      semaphore.release();
+      if (!semaphore.tryAcquire(TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
+        throw new SqlFormatException(-1, Buffer.buffer("Timeout waiting for formatter"));
+      }
+      try {
+        return formatWithProcess(input, options);
+      } finally {
+        semaphore.release();
+      }
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      throw new SqlFormatException(-1, Buffer.buffer("Interrupted while formatting"));
     }
   }
 
-  private Buffer format0(@NonNull Buffer input, @NonNull Options options)
+  private Buffer formatWithProcess(@NonNull Buffer input, @NonNull Options options)
       throws SqlFormatException, IOException, InterruptedException {
-    ProcessBuilder processBuilder = new ProcessBuilder();
-    processBuilder.command(buildCommand(options));
-    processBuilder.redirectErrorStream(false);
-    setClickhouseHome(processBuilder);
+    List<String> command = buildCommand(options);
+    Process process = createProcessBuilder(command).start();
 
-    Process process = processBuilder.start();
+    try {
+      writeInput(process, input);
+      if (!process.waitFor(TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
+        process.destroyForcibly();
+        throw new SqlFormatException(-1, Buffer.buffer("Process timeout"));
+      }
+
+      int exitCode = process.exitValue();
+      if (exitCode != 0) {
+        try (InputStream errorStream = process.getErrorStream()) {
+          throw new SqlFormatException(exitCode, inputStreamToBuffer(errorStream));
+        }
+      }
+
+      try (InputStream inputStream = process.getInputStream()) {
+        return inputStreamToBuffer(inputStream);
+      }
+    } finally {
+      process.destroyForcibly();
+    }
+  }
+
+  private void writeInput(Process process, Buffer input) throws IOException {
     try (OutputStream outputStream = process.getOutputStream()) {
       outputStream.write(input.getBytes());
-    }
-    if (!process.waitFor(30, TimeUnit.SECONDS)) {
-      process.destroy();
-      throw new SqlFormatException(-1, Buffer.buffer("Timeout"));
-    }
-    int code = process.exitValue();
-    if (code != 0) {
-      try (InputStream inputStream = process.getErrorStream()) {
-        throw new SqlFormatException(code, inputStreamToBuffer(inputStream));
-      }
-    }
-    try (InputStream inputStream = process.getInputStream()) {
-      return inputStreamToBuffer(inputStream);
+      outputStream.flush();
     }
   }
 
